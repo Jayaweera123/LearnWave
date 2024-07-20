7083c6a0bcc51f13372cb5e6b4397d07a10f085b
Jayaweera123 <155569167+Jayaweera123@users.noreply.github.com>
2024-07-20 10:16:29 +0530

    react app

---
+++ Client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-xl font-bold text-red-800'> Hellow Tailwind css with React Vite</h1>
+   
+    </>
+  )
+}
+
+export default App
